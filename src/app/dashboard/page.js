0b36d07337ee928240adba773348f1c3542a2df1
annapurnakhakhra <annapurnakhakhra@gmail.com>
2026-01-17 14:38:49 +0530

    17-01-2026(2)

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -13,7 +13,7 @@ import { jwtDecode } from "jwt-decode";
 
 export default function Dashboard() {
   const router = useRouter();
-  const API_URL = "https://adminrocket.megascale.co.in  ";
+  const API_URL = "https://adminrocket.megascale.co.in";
 
   const [currentTime, setCurrentTime] = useState(new Date());
   const [dashboardData, setDashboardData] = useState(null);
